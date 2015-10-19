b600cf2b1fd846118de3ce42e59103a5a907c95c
Kosy Anyanwu <kosy.anyanwu@andela.com>
2015-10-19 20:03:38 +0100

    Add setUp method for TestProcessMessage class

--- test.py
+++ test.py
@@ -6,14 +6,18 @@ from plugins.food_bot_plugin import process_message, Responses, outputs
 
 class TestProcessMessage(unittest.TestCase):
 
+    def setUp(self):
+        self.data = {}
+        self.data['channel'] = u'D0CLHTW20'
+        self.data['user'] = u'U0774N56J'
+
     @mock.patch('plugins.food_bot_plugin.Responses')
     def test_show_help_was_called(self, mock_response):
-        data = {}
-        data['channel'] = u'D0CLHTW20'
+        data = self.data
         data['text'] = u'help'
-        data['user'] = u'U0774N56J'
 
         process_message(data)
+
         self.assertTrue(mock_response.show_help.called)
         self.assertFalse(mock_response.show_menu.called)
         self.assertFalse(mock_response.rate.called)
@@ -23,12 +27,11 @@ class TestProcessMessage(unittest.TestCase):
 
     @mock.patch('plugins.food_bot_plugin.Responses')
     def test_show_menu_was_called(self, mock_response):
-        data = {}
-        data['channel'] = u'D0CLHTW20'
+        data = self.data
         data['text'] = u'menu'
-        data['user'] = u'U0774N56J'
 
         process_message(data)
+
         self.assertTrue(mock_response.show_menu.called)
         self.assertFalse(mock_response.show_help.called)
         self.assertFalse(mock_response.rate.called)
@@ -38,12 +41,11 @@ class TestProcessMessage(unittest.TestCase):
 
     @mock.patch('plugins.food_bot_plugin.Responses')
     def test_rate_was_called(self, mock_response):
-        data = {}
-        data['channel'] = u'D0CLHTW20'
+        data = self.data
         data['text'] = u'rate'
-        data['user'] = u'U0774N56J'
 
         process_message(data)
+
         self.assertTrue(mock_response.rate.called)
         self.assertFalse(mock_response.show_help.called)
         self.assertFalse(mock_response.show_menu.called)
@@ -53,12 +55,11 @@ class TestProcessMessage(unittest.TestCase):
 
     @mock.patch('plugins.food_bot_plugin.Responses')
     def test_comment_was_called(self, mock_response):
-        data = {}
-        data['channel'] = u'D0CLHTW20'
+        data = self.data
         data['text'] = u'comment'
-        data['user'] = u'U0774N56J'
 
         process_message(data)
+
         self.assertTrue(mock_response.enter_comment.called)
         self.assertFalse(mock_response.show_help.called)
         self.assertFalse(mock_response.show_menu.called)
@@ -68,12 +69,11 @@ class TestProcessMessage(unittest.TestCase):
 
     @mock.patch('plugins.food_bot_plugin.Responses')
     def test_get_rating_was_called(self, mock_response):
-        data = {}
-        data['channel'] = u'D0CLHTW20'
+        data = self.data
         data['text'] = u'get-rating'
-        data['user'] = u'U0774N56J'
 
         process_message(data)
+
         self.assertTrue(mock_response.get_average_ratings.called)
         self.assertFalse(mock_response.show_help.called)
         self.assertFalse(mock_response.show_menu.called)
@@ -83,12 +83,11 @@ class TestProcessMessage(unittest.TestCase):
 
     @mock.patch('plugins.food_bot_plugin.Responses')
     def test_show_error_was_called_with_wrong_command(self, mock_response):
-        data = {}
-        data['channel'] = u'D0CLHTW20'
+        data = self.data
         data['text'] = u'asdf'
-        data['user'] = u'U0774N56J'
 
         process_message(data)
+
         self.assertTrue(mock_response.show_error.called)
         self.assertFalse(mock_response.show_help.called)
         self.assertFalse(mock_response.show_menu.called)
@@ -101,6 +100,7 @@ class TestHelpFunction(unittest.TestCase):
 
     def test_help_returns_correct_string(self):
         Responses.show_help(u'D0CLHTW20')
+
         self.assertEqual(len(outputs), 1)
         self.assertEqual(type(outputs[0]), list)
         self.assertEqual(outputs[0][0], u'D0CLHTW20')
