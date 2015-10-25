1826556c111f75acc5b16aae92cabbdcd3b6113d
Kayode Adeniyi <kayode.adeniyi@andela.co>
2015-10-25 22:05:40 +0100

    Code cleanup, separate helper function into Helper class

--- plugins/food_bot_plugin.py
+++ plugins/food_bot_plugin.py
@@ -30,13 +30,13 @@ def process_message(data):
         if 'reply_to' in data and data['reply_to'] is None:
             return
 
-        elif text_buffer == 'help':
+        elif text_buffer == 'help' and len(buff) == 1:
             Response.show_help(channel)
 
         elif text_buffer == 'menu':
             Response.show_menu(channel, buff)
 
-        elif text_buffer == 'rate':
+        elif text_buffer == 'rate' and len(buff) > 3:
             Response.rate(channel, buff, user_id)
 
         elif text_buffer == 'get-rating':
@@ -46,61 +46,25 @@ def process_message(data):
             Response.show_error(channel)
 
 
-def get_day_of_week():
-    return datetime.datetime.now().strftime('%A').lower()
-
-
-def get_date():
-    return datetime.datetime.now().strftime("%A %b %d %Y")
-
-
-def get_week_number():
-    week = (datetime.datetime.now().isocalendar()[1] % 2) + 1
-    # fall back code here
-    # if week == 0:
-    #     week = 2
-    return week
-
-
-def check_meal_selected(meal):
-    if meal.lower() != 'breakfast' and meal.lower() != 'lunch':
-        return {'bool': False}
-    else:
-        return {'bool': True }
-
-def check_option_selected(option, day, week, meal):
-    variables = (meal, day, week,)
-    sql = CustomSQL()
-
-    query_string = "SELECT count(option) FROM menu_table WHERE meal = (%s) AND day = (%s) AND week = (%s)"
-    option_count_sql = sql.query(query_string, variables)
-    option_count = int(option_count_sql[0][0])
-
-    try:
-        if int(option) > option_count:
-            return {'bool': False, 'option': option_count}
-    except ValueError:
-        return {'bool': False, 'option': option_count}
-    else:
-        return {'bool': True }
-
-def check_rating(rating_val):
-    try:
-        rating = int(rating_val)
-        if rating not in range(1, 6):
-            return {'bool': False}
-        else:
-            return {'bool': True }
-    except ValueError:
-        return {'bool': False}
-
-
-
-class Response:
+class Helper:
+    """
+    Contains helper functions for validation and and sending the right context
+    """
+    @staticmethod
+    def get_day_of_week():
+        return datetime.datetime.now().strftime('%A').lower()
 
     @staticmethod
-    def show_help(channel):
-        return send_response('help_response', channel)
+    def get_date():
+        return datetime.datetime.now().strftime("%A %b %d %Y")
+
+    @staticmethod
+    def get_week_number():
+        week = (datetime.datetime.now().isocalendar()[1] % 2) + 1
+        # fall back code here
+        # if week == 0:
+        #     week = 2
+        return week
 
     @staticmethod
     def convert_menu_list_to_dict(menu):
@@ -118,24 +82,24 @@ class Response:
     @staticmethod
     def get_menu_template_context(buff):
         """
-        return the template name and correct context
+        return the template name and correct context for menu
         """
         if len(buff) == 1:
-            day = get_day_of_week()
+            day = Helper.get_day_of_week()
 
         if len(buff) > 1:
             day = buff[1].lower()
 
         if day in ['monday', 'tuesday', 'wednesday', 'thursday', 'friday']:
             sql = CustomSQL()
-            week = get_week_number()
+            week = Helper.get_week_number()
 
             variables = (day, week,)
             query_string = "SELECT food, meal, option FROM menu_table WHERE day = (%s) AND week = (%s)"
             menu = sql.query(query_string, variables)
 
             if menu:
-                menu_dict = Response.convert_menu_list_to_dict(menu)
+                menu_dict = Helper.convert_menu_list_to_dict(menu)
                 return {'template': 'menu_response',
                         'context': {'menu': menu_dict}}
 
@@ -146,50 +110,95 @@ class Response:
             return {'template': 'invalid_day_error', 'context': {}}
 
     @staticmethod
+    def check_meal_selected(meal):
+        if meal.lower() != 'breakfast' and meal.lower() != 'lunch':
+            return {'bool': False}
+        else:
+            return {'bool': True }
+
+    @staticmethod
+    def check_option_selected(option, day, week, meal):
+        variables = (meal, day, week,)
+        sql = CustomSQL()
+
+        query_string = "SELECT count(option) FROM menu_table WHERE meal = (%s) AND day = (%s) AND week = (%s)"
+        option_count_sql = sql.query(query_string, variables)
+        option_count = int(option_count_sql[0][0])
+
+        try:
+            if int(option) > option_count:
+                return {'bool': False, 'option': option_count}
+        except ValueError:
+            return {'bool': False, 'option': option_count}
+        else:
+            return {'bool': True }
+
+    @staticmethod
+    def check_rating(rating_val):
+        try:
+            rating = int(rating_val)
+            if rating not in range(1, 6):
+                return {'bool': False}
+            else:
+                return {'bool': True }
+        except ValueError:
+            return {'bool': False}
+
+    @staticmethod
+    def get_rate_template_context(buff, user_id):
+        """
+        return the template name and correct context for rating and comment
+        """
+        day = Helper.get_day_of_week()
+        # day = 'monday'
+        week = Helper.get_week_number()
+        if day in ['saturday', 'sunday']:
+            return {'template': 'weekend_rate_error', 'context': {}}
+
+        else:
+            meal = buff[1]
+            option = buff[2]
+            rating = buff[3]
+            comment = " ".join(buff[4:]) or "no comment"
+
+            check_option = Helper.check_option_selected(option, day, week, meal)
+
+            if Helper.check_meal_selected(meal)['bool'] is False:
+                return {'template': 'invalid_meal', 'context': {}}
+
+            if check_option['bool'] is False:
+                return {'template': 'invalid_option', 'context': {'option_count': check_option['option']}}
+
+            if Helper.check_rating(rating)['bool'] is False:
+                return {'template': 'invalid_rating', 'context': {}}
+
+            variables = (meal, 'tuesday', week, option,)
+            sql = CustomSQL()
+            query_string = "SELECT id FROM menu_table WHERE meal = (%s) AND day = (%s) AND week = (%s) AND option = (%s) "
+            result = sql.query(query_string, variables)
+            food_menu_id = int(result[0][0])
+
+            variables = (datetime.date(2005, 11, 18), user_id, food_menu_id, rating, comment)
+            query_string = "INSERT INTO rating (date, user_id, menu_id, rate, comment) VALUES (%s, %s, %s, %s, %s)"
+            sql.command(query_string, variables)
+            return {'template': 'rating_response', 'context': {}}
+
+
+class Response:
+
+    @staticmethod
+    def show_help(channel):
+        return send_response('help_response', channel)
+
+    @staticmethod
     def show_menu(channel, buff):
-        show_menu_dict = Response.get_menu_template_context(buff)
+        show_menu_dict = Helper.get_menu_template_context(buff)
         send_response(show_menu_dict['template'], channel,
                       show_menu_dict['context'])
 
     @staticmethod
-    def get_rate_template_context(buff, user_id):
-        # day = get_day_of_week()
-        day = 'monday'
-        week = get_week_number()
-        # if day in ['saturday', 'sunday']:
-        #     send_response('weekend_meal_error', channel)
-        # else:
-        meal = buff[1]
-        option = buff[2]
-        rating = buff[3]
-        comment = " ".join(buff[4:]) or "no comment"
-
-        check_option = check_option_selected(option, day, week, meal)
-
-        if check_meal_selected(meal)['bool'] is False:
-            return {'template': 'invalid_meal', 'context': {}}
-
-        if check_option['bool'] is False:
-            return {'template': 'invalid_option', 'context': {'option_count': check_option['option']}}
-
-        if check_rating(rating)['bool'] is False:
-            return {'template': 'invalid_rating', 'context': {}}
-
-        variables = (meal, 'tuesday', week, option,)
-        sql = CustomSQL()
-        query_string = "SELECT id FROM menu_table WHERE meal = (%s) AND day = (%s) AND week = (%s) AND option = (%s) "
-        result = sql.query(query_string, variables)
-        food_menu_id = int(result[0][0])
-
-        variables = (datetime.date(2005, 11, 18), user_id, food_menu_id, rating, comment)
-        query_string = "INSERT INTO rating (date, user_id, menu_id, rate, comment) VALUES (%s, %s, %s, %s, %s)"
-        sql.command(query_string, variables)
-        return {'template': 'rating_response', 'context': {}}
-
-
-    @staticmethod
     def rate(channel, buff, user_id):
-        rate_context_dict = Response.get_rate_template_context(buff, user_id)
+        rate_context_dict = Helper.get_rate_template_context(buff, user_id)
         send_response(rate_context_dict ['template'], channel, rate_context_dict['context'])
 
     @staticmethod
--- tests/show_menu_test.py
+++ tests/show_menu_test.py
@@ -1,6 +1,6 @@
 import unittest
 from mock import patch
-from plugins.food_bot_plugin import Response
+from plugins.food_bot_plugin import Response, Helper
 from custom_sql import CustomSQL
 
 
@@ -34,8 +34,8 @@ class TestShowMenu(unittest.TestCase):
                 '2': 'coconut rice and coleslaw'
             }
         }
-        sorted_menu_dict = Response.convert_menu_list_to_dict(self.sorted_menu)
-        unsorted_menu_dict = Response.convert_menu_list_to_dict(self.unsorted_menu)
+        sorted_menu_dict = Helper.convert_menu_list_to_dict(self.sorted_menu)
+        unsorted_menu_dict = Helper.convert_menu_list_to_dict(self.unsorted_menu)
 
         def compare_menu_dict(menu_dict1, menu_dict2):
             for meal_time in menu_dict1:
@@ -49,21 +49,21 @@ class TestShowMenu(unittest.TestCase):
         compare_menu_dict(unsorted_menu_dict, expected_dict)
         compare_menu_dict(expected_dict, unsorted_menu_dict)
 
-    @patch.object(Response, 'convert_menu_list_to_dict',
+    @patch.object(Helper, 'convert_menu_list_to_dict',
                   return_value="menu list as dict")
     @patch.object(CustomSQL, 'query', return_value="custom sql query")
-    @patch('plugins.food_bot_plugin.get_day_of_week', return_value="monday")
-    @patch('plugins.food_bot_plugin.get_week_number', return_value="1")
+    @patch.object(Helper, 'get_day_of_week', return_value="monday")
+    @patch.object(Helper, 'get_week_number', return_value="1")
     def test_correct_template_name_and_context(self, *args):
-        invalid_day_response_dict = Response.get_menu_template_context(['menu',
+        invalid_day_response_dict = Helper.get_menu_template_context(['menu',
                                                                        'asdf'])
-        weekend_response_dict = Response.get_menu_template_context(['menu',
+        weekend_response_dict = Helper.get_menu_template_context(['menu',
                                                                    'saturday'])
-        weekend_response_dict2 = Response.get_menu_template_context(['menu',
+        weekend_response_dict2 = Helper.get_menu_template_context(['menu',
                                                                     'SUNDAY'])
-        weekday_response_dict = Response.get_menu_template_context(['menu',
+        weekday_response_dict = Helper.get_menu_template_context(['menu',
                                                                    'tuesday'])
-        menu_response_dict = Response.get_menu_template_context(['menu'])
+        menu_response_dict = Helper.get_menu_template_context(['menu'])
 
         self.assertEqual(invalid_day_response_dict,
                          {'template': 'invalid_day_error', 'context': {}})
