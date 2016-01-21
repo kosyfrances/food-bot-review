00f4a954c184bfbff8818e42ac5418f19f0068ac
Samuel James <samuel.james@andela.com>
2016-01-21 10:56:53 +0100

    comment out database username and password

--- django_foodbot/django_foodbot/settings/development.py
+++ django_foodbot/django_foodbot/settings/development.py
@@ -8,8 +8,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'food_bot',
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
+        # 'USER': os.getenv('DB_USER'),
+        # 'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
