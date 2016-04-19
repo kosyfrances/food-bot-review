a83980c9871b2af75f95d4b5498f8349baf7f966
Kosy Anyanwu <kosy.anyanwu@andela.com>
2016-04-19 15:24:23 +0100

    Code cleanup, Add tests to check that config week is only A or B

---
+++ tests/helper_methods_test.py
@@ -0,0 +1,22 @@
+import os
+import unittest
+from config import Config
+
+
+class HelperMethodsTest(unittest.TestCase):
+
+    def test_that_config_week_is_only_A_or_B(self):
+        """
+        This test ensures that the only value set for 'WEEK'
+        in the envvars is A or B.
+        """
+        config = Config()
+
+        if os.path.exists('./rtmbot.conf'):
+            config.load_yaml('rtmbot.conf')
+
+        else:
+            config.load_os_environ_vars('FB__')
+
+        week_options = ['A', 'B']
+        self.assertIn(config['WEEK'], week_options)
